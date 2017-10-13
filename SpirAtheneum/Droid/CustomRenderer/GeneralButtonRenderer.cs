fe06c40ab31104c9f8d14c91f239436d8660ea01
waqasmunawer007 <waqasmunawer007@gmail.com>
2017-10-13 13:31:57 +0500

    Login screen UI refectoring done

---
+++ SpirAtheneum/Droid/CustomRenderer/GeneralButtonRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using SpirAtheneum.Renderers;
+using Xamarin.Forms.Platform.Android;
+using SpirAtheneum.Droid.CustomRenderer;
+
+[assembly: ExportRenderer(typeof(GeneralButton), typeof(GeneralButtonRenderer))]
+namespace SpirAtheneum.Droid.CustomRenderer
+{
+    class GeneralButtonRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.SetBackgroundResource(Resource.Drawable.general_rounded_button);
+            }
+        }
+    }
+}
---
+++ SpirAtheneum/iOS/CustomRenderer/LoginEntryRenderer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using CoreAnimation;
+using CoreGraphics;
+using SpirAtheneum.iOS.CustomRenderer;
+using SpirAtheneum.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(LoginEntry), typeof(LoginEntryRenderer))]
+
+namespace SpirAtheneum.iOS.CustomRenderer
+{
+    public class LoginEntryRenderer:EntryRenderer
+    {
+		private CALayer _line;
+		protected override void OnElementChanged(ElementChangedEventArgs<Entry> args)
+		{
+			base.OnElementChanged(args);
+			if (this.Control == null) return;
+
+
+            _line = null;
+			UITextField textField = (UITextField)Control;
+
+			// Most commonly customized for font and no border
+			//textField.Font = UIFont.FromName(DXStyle.FontFamily, (float)DXStyle.FontSmall.Size);
+			Control.BorderStyle = UITextBorderStyle.None;
+
+            _line = new CALayer
+            {
+                BorderColor = UIColor.FromRGB(126, 49, 59).CGColor,
+               
+				BackgroundColor = UIColor.FromRGB(126, 49, 59).CGColor,
+                //Frame = new CGRect(0, 5, Frame.Width * 2, 1f)
+                Frame = new CGRect(0, (Frame.Height / 2), Frame.Width * 1.06, 5f)
+			};
+
+			Control.Layer.AddSublayer(_line);
+			//// Use 'Done' on keyboard
+			//textField.ReturnKeyType = UIReturnKeyType.Done;
+			//textField.EnablesReturnKeyAutomatically = true;
+
+			//// No auto-correct
+			//textField.AutocorrectionType = UITextAutocorrectionType.No;
+			//textField.SpellCheckingType = UITextSpellCheckingType.No;
+			//textField.AutocapitalizationType = UITextAutocapitalizationType.Words;
+
+			//// Misc.
+			//textField.ClearButtonMode = UITextFieldViewMode.WhileEditing;
+			//textField.ClearsOnBeginEditing = false;
+			//textField.ClearsOnInsertion = false;
+			//textField.AdjustsFontSizeToFitWidth = false;
+			//textField.KeyboardAppearance = UIKeyboardAppearance.Default;
+		}
+    }
+}
