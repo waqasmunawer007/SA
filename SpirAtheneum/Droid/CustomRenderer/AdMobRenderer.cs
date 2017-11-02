24f9f3af3eb2ed75b414a16ff409d7d26e6c5fb8
waqar <waqar.ahmed5571@gmail.com>
2017-11-02 19:20:53 +0500

    added some changes

--- SpirAtheneum/Droid/CustomRenderer/AdMobRenderer.cs
+++ SpirAtheneum/Droid/CustomRenderer/AdMobRenderer.cs
@@ -1,39 +1,40 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-//using Android.App;
-//using Android.Content;
-//using Android.OS;
-//using Android.Runtime;
-//using Android.Views;
-//using Android.Widget;
-//using SpirAtheneum.Droid.CustomRenderer;
-//using Xamarin.Forms;
-//using SpirAtheneum.Renderers;
-//using Xamarin.Forms.Platform.Android;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using SpirAtheneum.Renderers;
+using SpirAtheneum.Droid.CustomRenderer;
+using Xamarin.Forms.Platform.Android;
+using Android.Gms.Ads;
 
-//[assembly: ExportRenderer(typeof(AdMob), typeof(AdMobRenderer))]
-//namespace SpirAtheneum.Droid.CustomRenderer
-//{
-//    class AdMobRenderer : ViewRenderer<AdMob, Android.Gms.Ads.AdView>
-//    {
-//        protected override void OnElementChanged(ElementChangedEventArgs<AdMob> e)
-//        {
-//            base.OnElementChanged(e);
+[assembly: ExportRenderer(typeof(AdMob), typeof(AdMobRenderer))]
+namespace SpirAtheneum.Droid.CustomRenderer
+{
+    public class AdMobRenderer : ViewRenderer<AdMob, AdView>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<AdMob> e)
+        {
+            base.OnElementChanged(e);
 
-//            if (Control == null)
-//            {
-//                var ad = new Android.Gms.Ads.AdView(Forms.Context);
-//                ad.AdSize = Android.Gms.Ads.AdSize.Banner;
-//                ad.AdUnitId = "_your_admob_ad_unit_id_goes_here_";
+            if (Control == null)
+            {
+                var ad = new AdView(Forms.Context);
+                ad.AdSize = AdSize.Banner;
+                ad.AdUnitId = "ca-app-pub-3940256099942544/6300978111";
 
-//                var requestbuilder = new Android.Gms.Ads.AdRequest.Builder();
-//                ad.LoadAd(requestbuilder.Build());
+                var requestbuilder = new AdRequest.Builder();
+                ad.LoadAd(requestbuilder.Build());
 
-//                SetNativeControl(ad);
-//            }
-//        }
-//    }
-//}
+                SetNativeControl(ad);
+            }
+        }
+    }
+}
